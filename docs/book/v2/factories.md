c94be492214986a22d53230df9babee25f79b54d
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:41:43 -0600

    Rewriting as Laminas Project package

--- docs/book/v2/factories.md
+++ docs/book/v2/factories.md
@@ -4,12 +4,12 @@
 creating [PSR-7](https://www.php-fig.org/psr/psr-7/) instances. As of version
 2.0.0, Diactoros supplies implementations of each as follows:
 
-- `Zend\Diactoros\RequestFactory`
-- `Zend\Diactoros\ResponseFactory`
-- `Zend\Diactoros\ServerRequestFactory`
-- `Zend\Diactoros\StreamFactory`
-- `Zend\Diactoros\UploadedFileFactory`
-- `Zend\Diactoros\UriFactory`
+- `Laminas\Diactoros\RequestFactory`
+- `Laminas\Diactoros\ResponseFactory`
+- `Laminas\Diactoros\ServerRequestFactory`
+- `Laminas\Diactoros\StreamFactory`
+- `Laminas\Diactoros\UploadedFileFactory`
+- `Laminas\Diactoros\UriFactory`
 
 The `ServerRequestFactory` continues to define the static method
 `fromGlobals()`, but also serves as a PSR-17 implementation.
--- src/Response/EmptyResponse.php
+++ src/Response/EmptyResponse.php
@@ -1,16 +1,17 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-diactoros for the canonical source repository
- * @copyright Copyright (c) 2015-2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-diactoros/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/laminas/laminas-diactoros for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-diactoros/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-diactoros/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\Diactoros\Response;
+namespace Laminas\Diactoros\Response;
 
-use Zend\Diactoros\Response;
-use Zend\Diactoros\Stream;
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\Stream;
 
 /**
  * A class representing empty HTTP responses.
